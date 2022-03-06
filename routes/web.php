95b54688f80a8622a8a3192d014858ce6c4808ac
Vladislav Nozhkin <mega.nozhkin@mail.ru>
2022-03-07 02:15:41 +0300

    added routers

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\NewsController;
+
+use App\Http\Controllers\Admin\CategoryController as AdminCategoryController;
+use App\Http\Controllers\Admin\NewsController as AdminNewsController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,14 +20,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [WelcomeController::class, 'index'])
+    -> name('welcome');
 
-Route::get('/info', function () {
-    return view('info');
-});
+Route::get('/categories', [CategoryController::class, "index"])
+    ->name('categories');
 
-Route::get('/news', function () {
-    return view('news');
+Route::get('/news/{category}', [NewsController::class, "index"])
+    ->name('news')
+    ->where(['category' => '[А-ЯЁа-яё]+']);
+
+Route::get('/news/{category}/{id}', [NewsController::class, "show"])
+    ->name('news.show')
+    ->where([
+        'id' => '\d+',
+        'category' => '[А-ЯЁа-яё]+',
+    ]);
+
+Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
+    Route::resource('category', AdminCategoryController::class);
+    Route::resource('news', AdminNewsController::class);
 });
