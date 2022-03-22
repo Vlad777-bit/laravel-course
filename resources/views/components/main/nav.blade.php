05325b39c78f03196c8dc3dee6e86b2214aa0fc9
Vladislav Nozhkin <mega.nozhkin@pm.me>
2022-03-22 19:01:19 +0300

    divided into components

---
+++ resources/views/components/main/nav.blade.php
@@ -0,0 +1,10 @@
+<nav class="nav">
+    <ul class="nav-list">
+        <li class="nav-item">
+            <a class="pure-button" href="{{ route('welcome') }}">Главная</a>
+        </li>
+        <li class="nav-item">
+            <a class="pure-button" href="{{ route('categories') }}">Категории</a>
+        </li>
+    </ul>
+</nav>
