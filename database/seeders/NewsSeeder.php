b06d43b388ecfb4160a66bae4135236dd521538a
Vladislav Nozhkin <mega.nozhkin@pm.me>
2022-04-04 01:20:47 +0300

    added seeders

---
+++ database/seeders/NewsSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Seeders;
+
+use Faker\Factory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class NewsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('News')->insert($this->getData());
+    }
+
+    private function getData(): array
+    {
+        $faker = Factory::create();
+        $newsData = [];
+
+        for ($i = 1; $i < 20; $i++) {
+            $newsData[] = [
+                'category_id' => 1,
+                'title' => $faker->jobTitle(),
+                'author' => $faker->userName(),
+                'image' => $faker->imageUrl(1250, 325),
+                'status' => 'ACTIVE',
+                'description' => $faker->text(500),
+            ];
+        }
+
+        return $newsData;
+    }
+}
