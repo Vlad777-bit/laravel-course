7bea93f01a5cef4c3dbe06af0a5e4be4ee6a3581
Vladislav Nozhkin <mega.nozhkin@pm.me>
2022-04-13 18:26:56 +0300

    added an error message

--- resources/views/components/ui/input.blade.php
+++ resources/views/components/ui/input.blade.php
@@ -1,4 +1,10 @@
 <div class="form-group">
     <label for="{{ $name }}">{{ $title }}</label>
-    <input type="{{ $type }}" name="{{ $name }}" id="{{ $name }}" value="{{ $value }}" class="form-control" />
+    <input
+        type="{{ $type }}"
+        name="{{ $name }}"
+        id="{{ $name }}"
+        value="{{ $value }}"
+        class="form-control @if($errors->has("$name")) alert-danger @endif" />
+    @error("$name") <strong class="text-danger">{{ $message }}</strong>  @enderror
 </div>
